36e5bdf7344c889f971d0723d661135ba31668db
Prasad <psam012@aucklanduni.ac.nz>
2019-08-06 18:38:13 +1200

    Renaming opencmiss_fields to opencmiss_tools

--- mesh_tools/__init__.py
+++ mesh_tools/__init__.py
@@ -3,7 +3,7 @@ from mesh_tools.exdata import *
 from mesh_tools.fields import *
 from mesh_tools.data import *
 try:
-    from mesh_tools.opencmiss_fields import *
+    from mesh_tools.opencmiss_tools import *
 except:
     pass
 from mesh_tools.mesh_conversions import *
